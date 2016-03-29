dd8b394117916becb0c13c66481df0c6fcc72919
Valeriy.Vyrva <valery1707@gmail.com>
2016-03-29 16:35:01 +0600

    Implement Sock.JS+STOMP connection mode At current time this work in broadcast mode - this is incorrect

--- src/main/java/name/valery1707/interview/websocket/app/StompController.java
+++ src/main/java/name/valery1707/interview/websocket/app/StompController.java
@@ -2,6 +2,7 @@ package name.valery1707.interview.websocket.app;
 
 import name.valery1707.interview.websocket.domain.WebProtocol;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import javax.inject.Inject;
@@ -13,7 +14,9 @@ public class StompController {
 	private MessageProcessor processor;
 
 	@MessageMapping("/stomp")
+	@SendTo("/topic/stomp")
 	public WebProtocol process(WebProtocol message) {
+		//todo At current time this work in broadcast mode - this is incorrect
 		return processor.process(message);
 	}
 }
