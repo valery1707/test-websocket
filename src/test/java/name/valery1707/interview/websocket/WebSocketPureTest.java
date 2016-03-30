f0a3edf36cb91070a5991f0e3ddc123492866b3a
Valeriy.Vyrva <valery1707@gmail.com>
2016-03-30 15:38:17 +0600

    Persist database in production mode

--- src/test/java/name/valery1707/interview/websocket/WebSocketPureTest.java
+++ src/test/java/name/valery1707/interview/websocket/WebSocketPureTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.socket.TextMessage;
@@ -32,6 +33,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringApplicationConfiguration(classes = Launcher.class)
 @WebAppConfiguration
 @IntegrationTest("server.port:0")
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class WebSocketPureTest {
 
 	@Inject
